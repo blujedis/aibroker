324db6a3d8f0cfc9ad0e8fe938b0782a8ef1533e
blujedis <blujedicorp@gmail.com>
2026-05-11 15:03:37 -0700

    make drizzle config paths absolute

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
 /// <reference types="node" />
 
 import { type Config, defineConfig } from 'drizzle-kit';
+import {join} from 'path';
+
+const cwd = process.cwd();
 
 const config: Config = {
-  schema: './src/lib/server/db/schema.postgres.ts',
-  out: './drizzle',
+  schema: join(cwd, './src/lib/server/db/schema.postgres.ts'),
+  out: join(cwd, './drizzle'),
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_CONNECTION_URL ?? 'postgresql://postgres:postgres@localhost:5432/aibroker'

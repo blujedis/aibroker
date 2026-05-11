046f1573731e66d2f1c667d5d396abd0b8a264bf
blujedis <blujedicorp@gmail.com>
2026-05-11 16:03:18 -0700

    update copy for drizzle in Dockerfile

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,10 @@
 /// <reference types="node" />
 
 import { type Config, defineConfig } from 'drizzle-kit';
-import {join} from 'path';
-
-const cwd = process.cwd();
 
 const config: Config = {
-  schema: join(cwd, './src/lib/server/db/schema.postgres.ts'),
-  out: join(cwd, './drizzle'),
+  schema: './src/lib/server/db/schema.postgres.ts',
+  out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_CONNECTION_URL ?? 'postgresql://postgres:postgres@localhost:5432/aibroker'
